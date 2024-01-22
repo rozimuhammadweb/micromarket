edaa86a425822dd05aeb578a24173e417d702fa1
brosoftware <ibrokhimovrozimuhammad@gmail.com>
2024-01-22 21:52:35 +0500

    new templates added

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,18 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback",
+        "plugins/fontawesome-free/css/all.min.css",
+        "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css",
+        "dist/css/adminlte.min.css",
+        "plugins/overlayScrollbars/css/OverlayScrollbars.min.css",
     ];
+
     public $js = [
+        "plugins/jquery-ui/jquery-ui.min.js",
+        "plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
+        "dist/js/adminlte.js",
+        "dist/js/pages/dashboard.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,24 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        "css/bootstrap.min.css",
+        "css/font-awesome.min.css",
+        "css/elegant-icons.css",
+        "css/nice-select.css",
+        "css/jquery-ui.min.css",
+        "css/owl.carousel.min.css",
+        "css/slicknav.min.css",
+        "css/style.css",
     ];
     public $js = [
+        "js/jquery-3.3.1.min.js",
+        "js/bootstrap.min.js",
+        "js/jquery.nice-select.min.js",
+        "js/jquery-ui.min.js",
+        "js/jquery.slicknav.js",
+        "js/mixitup.min.js",
+        "js/owl.carousel.min.js",
+        "js/main.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
